83888a5c86482e4e62c8350fa40dc1982a4949e5
brunocesarti <opusdey@132015>
2021-04-27 18:35:29 -0300

    Criando Collection Produtos com banco de dados Mongo #5

--- app-estoque/server/main.js
+++ app-estoque/server/main.js
@@ -1,9 +1,14 @@
 import { Meteor } from 'meteor/meteor';
+import { ProdutosCollection } from '/imports/api/ProdutosCollection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
+const insertProduto = produtoText => ProdutosCollection.insert({ text: produtoText });
 
 Meteor.startup(() => {
-
+  if (ProdutosCollection.find().count() === 0) {
+    [
+        {nome: 'Celular Motorola', quantidade: 3, valor: 1000},
+        {nome: 'Tablet Sansung', quantidade: 2, valor: 900},
+        {nome: 'Notebook Acer', quantidade: 1, valor: 3000},
+    ].forEach(insertProduto)
+  }
 });
