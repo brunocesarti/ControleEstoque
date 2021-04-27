c62e86c5e48ff5c2a9ab9e2b753a160c6570b624
brunocesarti <opusdey@132015>
2021-04-27 20:51:13 -0300

    Retornando visualização de produtos do BD #7

--- app-estoque/imports/ui/Produto.jsx
+++ app-estoque/imports/ui/Produto.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Produto = ({ produto }) => {
-    return <li>{produto.text.nome} | {produto.text.quantidade} | {produto.text.valor}</li>
+    return <ul>{produto.nome} | {produto.quantidade} | {produto.valor}</ul>
 };
