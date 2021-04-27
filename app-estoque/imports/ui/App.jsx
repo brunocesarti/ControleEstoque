f3cf32dded6fa0972b036ca79db7e429ce32db5a
brunocesarti <opusdey@132015>
2021-04-27 19:59:55 -0300

    Adicionando componente formulario de produto na tela inicial #4

--- app-estoque/imports/ui/App.jsx
+++ app-estoque/imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { ProdutosCollection } from '/imports/api/ProdutosCollection';
 import { Produto } from './Produto';
+import { ProdutoForm } from './ProdutoForm';
 
 export const App = () => {
     const produtos = useTracker(() => ProdutosCollection.find({}).fetch());
@@ -9,6 +10,9 @@ export const App = () => {
   return (
     <div>
       <h1>Controle de estoque - vs 1.0</h1>
+
+        <ProdutoForm/>
+
       <ul>
           { produtos.map(produto => <Produto key={ produto._id } produto={ produto }/>) }
       </ul>
