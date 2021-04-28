10d3ca94ebd45de7a6204c098c3769e57d682062
brunocesarti <opusdey@132015>
2021-04-28 10:25:59 -0300

    Atualizando função delete #7

--- app-estoque/imports/ui/Produto.jsx
+++ app-estoque/imports/ui/Produto.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Produto = ({ produto, onDeleteClick }) => {
-    return <ul>{produto.nome} | {produto.quantidade} | {produto.valor} <button onClick={ () => onDeleteClick(task) }>&times;</button></ul>
+    return <ul>{produto.nome} | {produto.quantidade} | {produto.valor} <button onClick={ () => onDeleteClick(produto) }>&times;</button></ul>
 };
