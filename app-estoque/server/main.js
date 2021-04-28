dc5155f58e9ac0aadfdf4faa95132dd6a7aca6b9
brunocesarti <opusdey@132015>
2021-04-28 10:55:52 -0300

    Atualizando base de dados para teste #7

--- app-estoque/server/main.js
+++ app-estoque/server/main.js
@@ -9,6 +9,10 @@ Meteor.startup(() => {
         { nome: 'Celular Motorola', quantidade: 3, valor: 1000 },
         { nome: 'Tablet Sansung', quantidade: 2, valor: 900 },
         { nome: 'Notebook Acer', quantidade: 1, valor: 3000 },
+        { nome: 'Celular Mi 9 plus', quantidade: 3, valor: 1350 },
+        { nome: 'Tablet Sansung A5', quantidade: 2, valor: 1900 },
+        { nome: 'Notebook Asus', quantidade: 1, valor: 4000 },
+        { nome: 'Notebook Dell', quantidade: 1, valor: 5000 },
     ].forEach(insertProduto)
   }
 });
